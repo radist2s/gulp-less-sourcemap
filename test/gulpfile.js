dad33bb751298e8dba7ebf5864f8f48cd9e40463
Alex Batalov <radist2s@gmail.com>
2015-12-26 16:18:51 +0530

    Test changed

--- test/gulpfile.js
+++ test/gulpfile.js
@@ -1,6 +1,5 @@
 var gulp = require('gulp');
 var less = require('../');
-var path = require('path');
 
 gulp.task('default', function () {
   gulp.src('./fixtures/buttons.less')
@@ -17,7 +16,40 @@ gulp.task('subfolder', function () {
     .pipe(less({
       sourceMap: {
         sourceMapRootpath: '../fixtures'
-      }
+      },
+      plugins: [
+        new LessPluginCleanCSS({
+          advanced: true
+        }),
+        new LessPluginAutoPrefix({
+          browsers: ['> 0%'],
+          cascade: true
+        })
+
+      ]
+    }))
+    .pipe(gulp.dest('./out'));
+});
+
+gulp.task('subfolder-and-plugins', function () {
+  var LessPluginCleanCSS = require('less-plugin-clean-css');
+  var LessPluginAutoPrefix = require('less-plugin-autoprefix');
+
+  gulp.src('./fixtures/*/**.less')
+    .pipe(less({
+      sourceMap: {
+        sourceMapRootpath: '../fixtures'
+      },
+      plugins: [
+        new LessPluginCleanCSS({
+          advanced: true
+        }),
+        new LessPluginAutoPrefix({
+          browsers: ['> 0%'],
+          cascade: true
+        })
+
+      ]
     }))
     .pipe(gulp.dest('./out'));
 });
--- test/main.js
+++ test/main.js
@@ -2,9 +2,8 @@ var should = require('should');
 var less = require('../');
 var gutil = require('gulp-util');
 var fs = require('fs');
-var pj = require('path').join;
-var os = require('os');
 var path = require('path');
+var pj = path.join;
 
 function createVinyl(lessFileName, contents) {
   var base = pj(__dirname, 'fixtures');
@@ -18,18 +17,20 @@ function createVinyl(lessFileName, contents) {
   });
 }
 
-describe('gulp-less-sourcemap', function () {
+describe('gulp-less', function () {
   describe('less()', function () {
+
     it('should pass file when it isNull()', function (done) {
       var stream = less();
       var emptyFile = {
         isNull: function () { return true; }
       };
-      stream.on('data', function (data) {
+      stream.once('data', function (data) {
         data.should.equal(emptyFile);
         done();
       });
       stream.write(emptyFile);
+      stream.end();
     });
 
     it('should emit error when file isStream()', function (done) {
@@ -38,84 +39,57 @@ describe('gulp-less-sourcemap', function () {
         isNull: function () { return false; },
         isStream: function () { return true; }
       };
-      stream.on('error', function (err) {
+      stream.once('error', function (err) {
         err.message.should.equal('Streaming not supported');
         done();
       });
       stream.write(streamFile);
+      stream.end();
     });
 
     it('should compile single less file', function (done) {
       var lessFile = createVinyl('buttons.less');
-      var stream = less();
-      stream.on('data', function (cssFile) {
-        if (path.basename(cssFile.path).match(/\.map$/)) {
-          return
-        }
 
+      var stream = less();
+      stream.once('data', function (cssFile) {
         should.exist(cssFile);
         should.exist(cssFile.path);
         should.exist(cssFile.relative);
         should.exist(cssFile.contents);
 
-        // replace End-of-line marker for cross OS testing
-        String(cssFile.contents).replace(/(\r\n|\n)/g, os.EOL).should.equal(
-          fs.readFileSync(pj(__dirname, 'expect/buttons.css'), 'utf8').replace(/(\r\n|\n)/g, os.EOL)
+        String(cssFile.contents).should.equal(
+          fs.readFileSync(pj(__dirname, 'expect/buttons.css'), 'utf8')
         );
         done();
       });
       stream.write(lessFile);
+      stream.end();
     });
 
     it('should emit error when less contains errors', function (done) {
+      var errorCalled = false;
       var stream = less();
       var errorFile = createVinyl('somefile.less',
         new Buffer('html { color: @undefined-variable; }'));
-      stream.on('error', function (err) {
+      stream.once('error', function (err) {
         err.message.should.equal('variable @undefined-variable is undefined in file '+errorFile.path+' line no. 1');
+        errorCalled = true;
+        errorCalled.should.equal(true);
+        done();
+      });
+      stream.once('end', function(){
+        errorCalled.should.equal(true);
         done();
       });
       stream.write(errorFile);
-    });
-
-    it('should continue to process next files when less error occurs', function (done) {
-      var stream = less();
-
-      var errorFile = createVinyl('somefile.less',
-        new Buffer('html { color: @undefined-variable; }'));
-      var normalFile = createVinyl('buttons.less');
-
-      var errorHandled = false;
-      var dataHandled = false;
-
-      stream.on('error', function (err) {
-        err.message.should.equal('variable @undefined-variable is undefined in file '+errorFile.path+' line no. 1');
-        errorHandled = true;
-        if (dataHandled) {
-          done();
-        }
-      });
-
-      var completedFiles = []
-
-      stream.on('data', function (cssFile) {
-        dataHandled = true;
-        if (errorHandled) {
-          if (/\.map$/.test(cssFile.path)) {
-            done();
-          }
-        }
-      });
-      stream.write(errorFile);
-      stream.write(normalFile);
+      stream.end();
     });
 
     it('should compile multiple less files', function (done) {
       var files = [
         createVinyl('buttons.less'),
         createVinyl('forms.less'),
-        createVinyl('normalize.less'),
-        createVinyl('subfolder/subfolder-buttons.less'),
+        createVinyl('normalize.less')
       ];
 
       var stream = less();
@@ -125,13 +99,92 @@ describe('gulp-less-sourcemap', function () {
         should.exist(cssFile.path);
         should.exist(cssFile.relative);
         should.exist(cssFile.contents);
-
         if (!--count) { done(); }
       });
 
       files.forEach(function (file) {
         stream.write(file);
       });
+      stream.end();
+    });
+
+    it('should produce sourcemap filenames and mappings', function (done) {
+      var files = [
+        createVinyl('buttons.less'),
+        createVinyl('forms.less'),
+        createVinyl('normalize.less')
+      ];
+
+      var stream = less();
+      var filesMaps = [];
+      stream.on('data', function (file) {
+        if (/.css.map$/.test(file.path)) {
+          filesMaps.push(path.basename(file.path));
+        }
+      });
+      stream.once('end', function () {
+        files.forEach(function (file) {
+          var fileName = path.basename(file.path).replace(/.less$/, '.css');
+          should(filesMaps.indexOf(fileName + '.map')).be.greaterThan(-1);
+        });
+
+        done();
+      });
+
+      files.forEach(function (file) {
+        stream.write(file);
+      });
+      stream.end();
+    });
+
+    it('should keep relativity to basedir', function (done) {
+      var lessFileSubPath = 'subfolder/subfolder-buttons.less';
+      var lessFile = createVinyl(lessFileSubPath);
+
+      var stream = less();
+      var outFiles = {};
+
+      stream.on('data', function (file) {
+        outFiles[file.path] = file;
+      });
+
+      stream.once('end', function () {
+        Object.keys(outFiles).forEach(function (cssFilePath) {
+          if (!/.css$/.test(cssFilePath)) {
+            return;
+          }
+
+          var mapFilePath = cssFilePath + '.map';
+          var mapFile = outFiles[mapFilePath];
+
+          should(mapFile).not.equal(undefined);
+
+          var sourcemap = JSON.parse(mapFile.contents.toString('utf-8'));
+          var targetSourceRegex = new RegExp(lessFileSubPath + '$');
+
+          var targetSourceFound = sourcemap.sources.some(function (sourceSubPath) {
+            if (!targetSourceRegex.test(sourceSubPath)) {
+              return;
+            }
+
+            var sourceBasedir = path.resolve(
+              path.dirname(lessFile.path),
+              sourceSubPath.replace(targetSourceRegex, '')
+            );
+
+            sourceBasedir.should.equal(lessFile.base);
+
+            return true;
+          });
+
+          should(targetSourceFound).equal(true);
+        });
+
+        done();
+      });
+
+      stream.write(lessFile);
+      stream.end();
     });
   });
 });
